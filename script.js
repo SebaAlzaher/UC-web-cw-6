cecb473ad8ee6f6542ae288c716abfcd8e017271
SebaAlzaher <107798418+SebaAlzaher@users.noreply.github.com>
2022-07-10 18:34:58 +0300

    don

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ let A_grades_div = document.getElementById("A-grades");
 let bonus_grades_div = document.getElementById("bonus-grades");
 let random_grade_div = document.getElementById("random-grade");
 
+grades.pop();
+
+grades.push(99);
+
+
+grades.forEach(
+  (grades)=>(all_grades_div.innerHTML+=`<span>${grades}</span>`)
+);
+
+
+for(let i=0;i<grades.length;i++)
+{
+  all_grades_div.innerHTML+=`<span>${grades[i]}</span>`
+}
 
 function search() {
   let inputValue = document.getElementById("input").value;
